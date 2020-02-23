641251fee56ebc3596007851969da9f9d0d21988
Nhawley <nhawley255@gmail.com>
2020-02-23 16:53:55 -0500

    Updates

---
+++ precache-manifest.0d6287cc93491cc03dd7b8c61cf8aa7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdbeb3705f98afe904ea272b925cfed",
+    "url": "/MarkStars/index.html"
+  },
+  {
+    "revision": "3ce92d942b256f140205",
+    "url": "/MarkStars/static/css/main.2c1a4a2e.chunk.css"
+  },
+  {
+    "revision": "d262147154402c77b98a",
+    "url": "/MarkStars/static/js/2.4cca7492.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MarkStars/static/js/2.4cca7492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce92d942b256f140205",
+    "url": "/MarkStars/static/js/main.37edbafd.chunk.js"
+  },
+  {
+    "revision": "42a498993bad5efb2569",
+    "url": "/MarkStars/static/js/runtime-main.fece37ce.js"
+  }
+]);
